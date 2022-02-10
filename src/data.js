f3f5e8d69c2352fe647070cb8d3d4dbcc14e7962
Kevin Virani <kevin.virani5@gmail.com>
2022-02-10 16:20:20 -0500

    second commit

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+    {
+        id: 1,
+        title: "SUMMER SALE",
+        img: "https://media.istockphoto.com/photos/young-attractive-happy-couple-hugging-smiling-and-holding-shopping-picture-id1277267181?b=1&k=20&m=1277267181&s=170667a&w=0&h=Ikff2jc6Trv2shwsCOJBIG8Rdgyd7VzWtI0Ihyk8Go8=",
+        desc: "DO NOT COMPROMISE ON STYLE",
+        bg: "f5fafd"
+    },
+
+    {
+        id: 2,
+        title: "Winter SALE",
+        img: "https://media.istockphoto.com/photos/young-attractive-happy-couple-hugging-smiling-and-holding-shopping-picture-id1277267181?b=1&k=20&m=1277267181&s=170667a&w=0&h=Ikff2jc6Trv2shwsCOJBIG8Rdgyd7VzWtI0Ihyk8Go8=",
+        desc: "DO NOT COMPROMISE ON STYLE",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        title: "POPULAR SALE",
+        img: "https://media.istockphoto.com/photos/young-attractive-happy-couple-hugging-smiling-and-holding-shopping-picture-id1277267181?b=1&k=20&m=1277267181&s=170667a&w=0&h=Ikff2jc6Trv2shwsCOJBIG8Rdgyd7VzWtI0Ihyk8Go8=",
+        desc: "DO NOT COMPROMISE ON STYLE",
+        bg: "f5fafd"
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        title: "SHIRT STYLE",
+        img: "https://images.ctfassets.net/371ugtyffwio/44jlsBMUobOggRYyko9Ody/b5ae3862ae61ff55832ab27e84b56984/Hero_M1_hero_whiteshirt-min.jpg",
+    },
+    {
+        id: 2,
+        title: "LOUNGEWEAR LOVE",
+        img: "https://images.ctfassets.net/371ugtyffwio/44jlsBMUobOggRYyko9Ody/b5ae3862ae61ff55832ab27e84b56984/Hero_M1_hero_whiteshirt-min.jpg",
+    },
+    {
+        id: 3,
+        title: "LIGHT JACKES",
+        img: "https://images.ctfassets.net/371ugtyffwio/44jlsBMUobOggRYyko9Ody/b5ae3862ae61ff55832ab27e84b56984/Hero_M1_hero_whiteshirt-min.jpg",
+    }
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://image.uniqlo.com/UQ/ST3/us/imagesother/home/220131-L1-SP-m-chambray.jpg"
+    },
+    {
+        id: 2,
+        img: "https://image.uniqlo.com/UQ/ST3/us/imagesother/home/220131-L1-SP-m-chambray.jpg"
+    },
+    {
+        id: 3,
+        img: "https://image.uniqlo.com/UQ/ST3/us/imagesother/home/220131-L1-SP-m-chambray.jpg"
+    },
+    {
+        id: 4,
+        img: "https://image.uniqlo.com/UQ/ST3/us/imagesother/home/220131-L1-SP-m-chambray.jpg"
+    },
+    {
+        id: 5,
+        img: "https://image.uniqlo.com/UQ/ST3/us/imagesother/home/220131-L1-SP-m-chambray.jpg"
+    },
+    {
+        id: 6,
+        img: "https://image.uniqlo.com/UQ/ST3/us/imagesother/home/220131-L1-SP-m-chambray.jpg"
+    },
+    {
+        id: 7,
+        img: "https://image.uniqlo.com/UQ/ST3/us/imagesother/home/220131-L1-SP-m-chambray.jpg"
+    },
+    {
+        id: 8,
+        img: "https://image.uniqlo.com/UQ/ST3/us/imagesother/home/220131-L1-SP-m-chambray.jpg"
+    }
+]
