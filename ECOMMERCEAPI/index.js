0c73a0e7aab09d74a70c3577790e911fea15f9e4
Kev2994 <kevinvirani5@gmail.com>
2022-02-12 12:46:58 -0500

    auth commit

--- ECOMMERCEAPI/index.js
+++ ECOMMERCEAPI/index.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const app = express();
-const dotenv= require("dotenv");
-const userRoute = require("./routes/user")
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 const mongoose = require("mongoose");
 
-dotenv.config(); 
-
-
+dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL)    
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("db connection successfull"))
   .catch((err) => console.log(err));
 
-  app.use(express.json());
-
+app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("app is running" + port);
--- ECOMMERCEAPI/routes/user.js
+++ ECOMMERCEAPI/routes/user.js
@@ -1,12 +1,26 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
 
-router.get("/usertest", (req,res)=>{
-    res.send("app is running")
-})
-
-router.post("/userposttest", (req,res) =>{
-    res.send("app is halting")
-})
-
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
